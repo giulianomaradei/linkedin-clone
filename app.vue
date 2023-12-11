e43308da852b11a32724ea29db16b2b8da41786c
giuliano <giulianomaradei@gmail.com>
2023-12-11 20:05:01 -0300

    Root component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -8,8 +8,14 @@
 
 <style>
   body{
-    background-color: #f4f2ee;
     margin: 0;
     padding: 0;
+    background-color: #f4f2ee;
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Fira Sans,Ubuntu,Oxygen,Oxygen Sans,Cantarell,Droid Sans,Apple Color Emoji,Segoe UI Emoji,Segoe UI Emoji,Segoe UI Symbol,Lucida Grande,Helvetica,Arial,sans-serif;
+    overflow-x: hidden;
+  }
+
+  .disable-scroll {
+    overflow: hidden;
   }
 </style>
